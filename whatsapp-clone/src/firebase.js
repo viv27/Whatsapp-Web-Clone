e1f7678162a1b085b5d4bafd42bb3491143422d6
Vivek Mohan <vivekmohan27@gmail.com>
2021-04-27 11:05:29 +0530

    feat: made basic tempelate

---
+++ whatsapp-clone/src/firebase.js
@@ -0,0 +1,12 @@
+
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyB-vMT3msD46463qJFk7TIvYWbKNKG2lo8",
+    authDomain: "whatsapp-clone-332d2.firebaseapp.com",
+    projectId: "whatsapp-clone-332d2",
+    storageBucket: "whatsapp-clone-332d2.appspot.com",
+    messagingSenderId: "700074838848",
+    appId: "1:700074838848:web:bc53a7afcfb2c93b7408a5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
