33bbcccf51b110b7679e2986632de332d3f3dd8a
Vivek Mohan <vivekmohan27@gmail.com>
2021-05-04 21:33:51 +0530

    Built Sidebar Avatar and Search Bar

--- whatsapp-clone/src/App.js
+++ whatsapp-clone/src/App.js
@@ -1,39 +1,18 @@
-import React, { useState } from 'react'
-import './App.css';
+import React from 'react';
+import './App.css'
 import Sidebar from './Sidebar';
-import Chat from './Chat'
-import Login from './Login'
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 
-function App() {
-  const [user,setUser] = useState(null)
-  return (
+
+function App(){
+  return(
     <div className="App">
-      {!user ? (<Login/>
-      ):(
-        <div className="app__body">
-        
-        <Router>
-          <Sidebar/>
-          <Switch>
-            
-            <Chat/>
-            <Route path = "/rooms/:roomId">
-              <Chat/>
-                
-            </Route>
-            <Route path ="/">
-               <Chat/>
-            </Route>
-            
-          </Switch>
-       </Router>
-      </div>
-
-      )}
       
+      <div className="app__body">
+        <Sidebar/>
+
+      </div>
     </div>
-  ) ;
+  )
 }
 
-export default App;
+export default App
--- whatsapp-clone/src/Sidebar.js
+++ whatsapp-clone/src/Sidebar.js
@@ -1,62 +1,48 @@
-import React,{useState,useEffect} from 'react'
-import './Sidebar.css'
-import { Avatar, IconButton,} from "@material-ui/core"
-import ChatIcon from '@material-ui/icons/Chat';
+import React from 'react'
+import {Avatar, IconButton} from "@material-ui/core"
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
-import SidebarChat from './SidebarChat'
-import db from './firebase';
+import {SearchOutlined} from '@material-ui/icons/SearchOutlined'; 
+import SearchIcon from '@material-ui/icons/Search';
+
+import './Sidebar.css'
+
 function Sidebar() {
-
-    const [rooms,setRooms] = useState([])
-
-    useEffect(()=>{
-        const unsubscribe = db.collection('rooms').onSnapshot(snap=>{
-            setRooms(snap.docs.map(doc=>
-                ({
-                    id:doc.id,
-                    data:doc.data()
-                })
-
-            ))
-        })
-        return()=>{
-            unsubscribe()
-        }
-    },[])
     return (
         <div className="sidebar">
             <div className="sidebar__header">
                 <Avatar/>
                 <div className="sidebar__headerRight">
                     <IconButton>
-                    <DonutLargeIcon/>
+                        <DonutLargeIcon/>
                     </IconButton>
+
                     <IconButton>
-                    <ChatIcon/>
+                        <ChatIcon/>
                     </IconButton>
+
                     <IconButton>
-                    <MoreVertIcon/>
+                        <MoreVertIcon/>
                     </IconButton>
+
+                    
+                    
                     
                 </div>
-
             </div>
             <div className="sidebar__search">
-                <div className="sidebar__searchContiner">
+                <div className="sidebar__searchContainer">
 
-                
-                <SearchOutlinedIcon/>
-                <input type="text" placeholder="Search or start a new chat" />
+                <SearchIcon/>
+                <input placeholder="Search or start a new Chat" type="text"/>
+
                 </div>
+                
+
             </div>
             <div className="sidebar__chats">
-                <SidebarChat addNewChat/>
-                {rooms.map(room=>(
-                    <SidebarChat key={room.id} id={room.id} name={room.data.name}/>
-                ))}
-                
+
             </div>
             
         </div>
